f8848b23bf685affb8a4f0b22de689c276e5bbd3
MLBoy <rockyshikoku@gmail.com>
2022-01-04 02:28:01 +0900

    Add files via upload

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 間嶋大輔 on 2022/01/04.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
